00982eaf9d0548c5d1827ca40b52477f91af798c
Julien VITTE <julien.vitte@insidegroup.fr>
2018-04-03 23:38:00 +0200

    Implements curry

---
+++ src/Curry.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Pitchart\Phunktional;
+
+/**
+ * Class Curry
+ *
+ * @package Pitchart\Phunktional
+ *
+ * @author Julien VITTE <vitte.julien@gmail.fr>
+ */
+class Curry
+{
+    /**
+     * @var callable
+     */
+    private $function;
+
+    /**
+     * Curry constructor.
+     *
+     * @param callable $function
+     */
+    public function __construct(callable $function)
+    {
+        $this->function = $function;
+    }
+
+    /**
+     * @return \Closure
+     */
+    public function __invoke(...$arguments)
+    {
+        $function = $this->function;
+        return function (...$args) use ($function, $arguments) {
+            return (function ($arguments) use ($function) {
+                if (count($arguments) < (new \ReflectionFunction($function))->getNumberOfRequiredParameters()) {
+                    return new self($function, ...$arguments);
+                }
+                return $function(...$arguments);
+            })(array_merge($arguments, $args));
+        };
+    }
+
+}
---
+++ tests/CurryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pitchart\Phunktional\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Pitchart\Phunktional\Curry;
+use Pitchart\Phunktional as t;
+
+class CurryTest extends TestCase
+{
+    public function test_object_curries_closures()
+    {
+        $plus = function ($x, $y) { return $x + $y; };
+        $curriedPlus = new Curry($plus);
+        $plusOne = $curriedPlus(1);
+
+        self::assertEquals($plus(1, 1), $plusOne(1));
+        self::assertEquals($plus(1, 1), $curriedPlus(1)(1));
+    }
+
+    public function test_object_curries_php_functions()
+    {
+        $curried = new Curry('implode');
+        self::assertEquals('abc', $curried('')(['a', 'b', 'c']));
+    }
+
+    public function test_helper_curries_closures()
+    {
+        $plus = function ($x, $y) { return $x + $y; };
+        $plusOne = t\curry($plus, 1);
+
+        self::assertEquals($plus(1, 1), $plusOne(1));
+    }
+
+    public function test_helper_curries_php_functions()
+    {
+        $curried = t\curry('implode', '');
+        self::assertEquals('abc', $curried(['a', 'b', 'c']));
+    }
+
+}
