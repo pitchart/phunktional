b70b7ca5c695a216fd8d9aa54581077339a00e83
Julien VITTE <julien.vitte@insidegroup.fr>
2018-07-31 10:40:37 +0200

    Reduces dependencies between array tests

--- tests/ArrayTest.php
+++ tests/ArrayTest.php
@@ -12,106 +12,140 @@ class ArrayTest extends TestCase
 
     public function test_supports_mapping()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $mapping = p\map(function (int $item) { return $item + 1; });
 
-        self::assertEquals([2, 4, 6, 10, 7, 5, 3, 4, 6, 7, 3], $mapping($this->array));
+        self::assertEquals([2, 4, 6, 10, 7, 5, 3, 4, 6, 7, 3], $mapping($array));
     }
 
     public function test_supports_filtering()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $filter = p\filter(function (int $item) { return $item %2 == 0; });
 
-        self::assertEquals([6, 4, 2, 6, 2], array_values($filter($this->array)));
+        self::assertEquals([6, 4, 2, 6, 2], array_values($filter($array)));
     }
 
     public function test_supports_rejection()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $rejection = p\reject(function (int $item) { return $item %2 == 0; });
 
-        self::assertEquals([1, 3, 5, 9, 3, 5], array_values($rejection($this->array)));
+        self::assertEquals([1, 3, 5, 9, 3, 5], array_values($rejection($array)));
     }
 
     public function test_supports_taking_the_firsts_elements()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $firsts = p\take(5);
 
-        self::assertEquals([1, 3, 5, 9, 6], $firsts($this->array));
+        self::assertEquals([1, 3, 5, 9, 6], $firsts($array));
     }
 
     public function test_supports_tailing()
     {
-        self::assertEquals([3, 5, 9, 6, 4, 2, 3, 5, 6, 2], p\tail()($this->array));
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        self::assertEquals([3, 5, 9, 6, 4, 2, 3, 5, 6, 2], p\tail()($array));
     }
 
     public function test_supports_slicing()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $slicing = p\slice(2, 4);
 
-        self::assertEquals([5, 9, 6, 4], $slicing($this->array));
+        self::assertEquals([5, 9, 6, 4], $slicing($array));
     }
 
     public function test_supports_dropping()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $dropping = p\drop(3);
 
-        self::assertEquals([9, 6, 4, 2, 3, 5, 6, 2], $dropping($this->array));
+        self::assertEquals([9, 6, 4, 2, 3, 5, 6, 2], $dropping($array));
     }
 
     public function test_supports_concatenation()
     {
-        self::assertEquals([1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2, 1, 2, 3, 4, 5, 6], p\concat([1, 2], [3, 4, 5, 6])($this->array));
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        $concatenation = p\concat([1, 2], [3, 4, 5, 6]);
+
+        self::assertEquals([1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2, 1, 2, 3, 4, 5, 6], $concatenation($array));
 
     }
 
     public function test_supports_mapping_and_flattening_the_result()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $flatmap = p\flatmap(function (int $item) { return [$item, $item + 1];});
 
-        self::assertEquals([1, 2, 3, 4, 5, 6, 9, 10, 6, 7, 4, 5, 2, 3, 3, 4, 5, 6, 6, 7, 2, 3], $flatmap($this->array));
+        self::assertEquals([1, 2, 3, 4, 5, 6, 9, 10, 6, 7, 4, 5, 2, 3, 3, 4, 5, 6, 6, 7, 2, 3], $flatmap($array));
     }
 
     public function test_supports_distinct()
     {
-        self::assertEquals([1, 3, 5, 9, 6, 4, 2], p\distinct()([1, 3, 5, 3, 9, 6, 4, 2, 3, 5, 6, 2]));
+        $array = [1, 3, 5, 3, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        self::assertEquals([1, 3, 5, 9, 6, 4, 2], p\distinct()($array));
     }
 
     public function test_supports_flatten()
     {
-        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9], p\flatten()([1, [2, [3, 4], 5], [[6, 7], 8], [9]]));
+        $array = [1, [2, [3, 4], 5], [[6, 7], 8], [9]];
+
+        self::assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9], p\flatten()($array));
     }
 
     public function test_supports_reducing()
     {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
         $sum = p\reduce(function (int $total, int $number) { return $total + $number; }, 0);
 
-        self::assertEquals(46, $sum($this->array));
+        self::assertEquals(46, $sum($array));
     }
 
     public function test_supports_intersections()
     {
-        self::assertEquals([1, 5, 9, 5], p\intersect([1, 7, 9, 5])($this->array));
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        self::assertEquals([1, 5, 9, 5], p\intersect([1, 7, 9, 5])($array));
     }
 
     public function test_supports_differences()
     {
-        self::assertEquals([3, 6, 4, 2, 3, 6, 2], p\diff([1, 7, 9, 5])($this->array));
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        self::assertEquals([3, 6, 4, 2, 3, 6, 2], p\diff([1, 7, 9, 5])($array));
     }
 
     public function test_supports_union()
     {
-        self::assertEquals([1, 3, 5, 9, 6, 4, 2, 7], p\union([1, 7, 9, 5])($this->array));
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        self::assertEquals([1, 3, 5, 9, 6, 4, 2, 7], p\union([1, 7, 9, 5])($array));
     }
 
     public function test_supports_sorting()
     {
-        $sorting = p\sort(function ($first, $second) {
+        $array = [1, 3, 5, 9, 6, 4, 2, 3, 5, 6, 2];
+
+        $sorting = p\sort(function (int $first, int $second) {
             if ($first != $second) {
                 return $first < $second ? -1 : 1;
             }
             return 0;
         });
 
-        self::assertEquals([1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 9], $sorting($this->array));
+        self::assertEquals([1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 9], $sorting($array));
     }
 
     /**
